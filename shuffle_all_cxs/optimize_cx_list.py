90bf664a1a0bbd0fa58cf21fa4419ebf0e63a4c9
peter-janderks <peter-janderks@hotmail.com>
2025-04-09 17:36:07 +0200

    replace sampler with sinter.collect

--- shuffle_all_cxs/optimize_cx_list.py
+++ shuffle_all_cxs/optimize_cx_list.py
@@ -5,12 +5,14 @@ from copy import deepcopy
 from typing import List, Dict, Tuple
 from draw_ordered_tanner_graph import draw_cx_list
 import stim
+import sinter
 import numpy as np
-from stimbposd import BPOSD
+from stimbposd import SinterDecoder_BPOSD, sinter_decoders
 import matplotlib.pyplot as plt
 
 # A CX gate is represented as a tuple (q, a)
 CXGate = Tuple[str, str]
+
 
 def objective(cx_list: List[CXGate],
               ancilla_type: Dict[str, str],
@@ -23,19 +25,19 @@ def objective(cx_list: List[CXGate],
     undetectable logical errors found by Stim's search_for_undetectable_logical_errors function.
     """
     _, circ = memory_experiment_circuit_from_cx_list(
-        cx_list = cx_list,
-        ancilla_type = ancilla_type,
-        data_mapping = data_mapping,
-        ancilla_mapping = ancilla_mapping,
-        flag_mapping = dict(),
-        lz = lz,
-        p = p,
-        x_detectors = False,
-        z_detectors = True,
-        cycles_before_noise = 1,
-        cycles_with_noise = 2,
-        cycles_after_noise = 1,
-        flag = False
+        cx_list=cx_list,
+        ancilla_type=ancilla_type,
+        data_mapping=data_mapping,
+        ancilla_mapping=ancilla_mapping,
+        flag_mapping=dict(),
+        lz=lz,
+        p=p,
+        x_detectors=False,
+        z_detectors=True,
+        cycles_before_noise=1,
+        cycles_with_noise=2,
+        cycles_after_noise=1,
+        flag=False
     )
 
     # try:
@@ -49,6 +51,7 @@ def objective(cx_list: List[CXGate],
 
     return len(errors), circ
 
+
 def objective_logical_error_rate(cx_list: List[CXGate],
                                  ancilla_type: Dict[str, str],
                                  data_mapping: Dict[str, int],
@@ -57,39 +60,44 @@ def objective_logical_error_rate(cx_list: List[CXGate],
                                  p_cx: float,
                                  p_idle: float,
                                  num_shots: int = 10000) -> float:
-        """
-        Build a circuit from the given cx_list and return an objective value computed as the logical error rate.
-        """
-        _, circ = memory_experiment_circuit_from_cx_list(
-            cx_list = cx_list,
-            ancilla_type = ancilla_type,
-            data_mapping = data_mapping,
-            ancilla_mapping = ancilla_mapping,
-            flag_mapping = dict(),
-            lz = lz,
-            p_cx = p_cx,
-            p_idle = p_idle,
-            x_detectors = False,
-            z_detectors = True,
-            cycles_before_noise = 1,
-            cycles_with_noise = 2,
-            cycles_after_noise = 1,
-            flag = False
-        )
+    """
+    Build a circuit from the given cx_list and return an objective value computed as the logical error rate.
+    """
+    _, circ = memory_experiment_circuit_from_cx_list(
+        cx_list=cx_list,
+        ancilla_type=ancilla_type,
+        data_mapping=data_mapping,
+        ancilla_mapping=ancilla_mapping,
+        flag_mapping=dict(),
+        lz=lz,
+        p_cx=p_cx,
+        p_idle=p_idle,
+        x_detectors=False,
+        z_detectors=True,
+        cycles_before_noise=1,
+        cycles_with_noise=2,
+        cycles_after_noise=1,
+        flag=False
+    )
 
-        sampler = circ.compile_detector_sampler()
-        syndromes, observables = sampler.sample(num_shots, separate_observables=True)
+    task = sinter.Task(
+        circuit=circ,
 
-        observables = observables[:, :lz.shape[0]]
+    )
 
-        decoder = BPOSD(
-            circ.detector_error_model(), max_bp_iters=20)
+    stats = sinter.collect(tasks=[task], num_workers=10,
+                           max_shots=10_000,
+                           max_errors=100,
+                           decoders=['bposd'],
+                           custom_decoders=sinter_decoders(),
+                           )
+    for stat in stats:
+        logical_error_rate = stat.errors / stat.shots
+        break
+    else:
+        logical_error_rate = 0.0
 
-        predicted_observables = decoder.decode_batch(syndromes)
-        mistakes = np.any(predicted_observables != observables, axis=1)
-        num_mistakes = np.sum(mistakes)
-
-        return num_mistakes / num_shots, circ
+    return logical_error_rate, circ
 
 
 def optimize_cx_list(
@@ -115,7 +123,8 @@ def optimize_cx_list(
     objectives_list = []
 
     best_cx_list = deepcopy(initial_cx_list)
-    best_obj, best_circ = objective_logical_error_rate(best_cx_list, ancilla_type, data_mapping, ancilla_mapping, lz, p_cx, p_idle, num_shots=num_shots)
+    best_obj, best_circ = objective_logical_error_rate(
+        best_cx_list, ancilla_type, data_mapping, ancilla_mapping, lz, p_cx, p_idle, num_shots=num_shots)
     print(f"Initial objective value: {best_obj}")
     objectives_list.append(best_obj)
 
@@ -124,14 +133,16 @@ def optimize_cx_list(
         changed = random_legal_local_change_inplace(candidate, ancilla_type)
         if not changed:
             continue
-        obj_val, _ = objective_logical_error_rate(candidate, ancilla_type, data_mapping, ancilla_mapping, lz, p_cx, p_idle, num_shots=num_shots)
+        obj_val, _ = objective_logical_error_rate(
+            candidate, ancilla_type, data_mapping, ancilla_mapping, lz, p_cx, p_idle, num_shots=num_shots)
         objectives_list.append(obj_val)
         if obj_val <= best_obj:
             best_cx_list = candidate
         if obj_val < best_obj:
             best_obj = obj_val
             print(f"Iteration {i}: improved objective to {best_obj}")
-            draw_cx_list(best_cx_list, ancilla_type, data_coords=data_coords, ancilla_coords=ancilla_coords)
+            draw_cx_list(best_cx_list, ancilla_type,
+                         data_coords=data_coords, ancilla_coords=ancilla_coords)
 
     plt.figure()
     plt.plot(objectives_list)
@@ -152,12 +163,12 @@ if __name__ == '__main__':
     data_coords = code.data_coords
     ancilla_coords = code.ancilla_coords
 
-    optimize_cx_list(initial_cx_list = cx_list,
-                     ancilla_type = ancilla_type,
-                     data_mapping = data_mapping,
-                     ancilla_mapping = ancilla_mapping,
-                     p_cx = 0.01,
-                     p_idle = 0.001,
-                     iterations = 1000,
-                     data_coords = data_coords,
-                     ancilla_coords = ancilla_coords)
+    optimize_cx_list(initial_cx_list=cx_list,
+                     ancilla_type=ancilla_type,
+                     data_mapping=data_mapping,
+                     ancilla_mapping=ancilla_mapping,
+                     p_cx=0.01,
+                     p_idle=0.001,
+                     iterations=1000,
+                     data_coords=data_coords,
+                     ancilla_coords=ancilla_coords)
