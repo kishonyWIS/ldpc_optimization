7423e25d7d077a4f04f443d31973b6acb477debc
peter-janderks <peter-janderks@hotmail.com>
2025-05-27 10:00:29 +0200

    update get_quits_codes

--- src/get_quits_codes.py
+++ src/get_quits_codes.py
@@ -1,11 +1,12 @@
 import stim
-from cx_list_from_stabilizers_in_sequence import StabilizerCode
+from src.cx_list_from_stabilizers_in_sequence import StabilizerCode
 from quits.qldpc_code import *
 from quits.circuit import get_qldpc_mem_circuit
 from quits.decoder import sliding_window_bposd_circuit_mem
 from quits.simulation import get_stim_mem_result
 
-def get_bpc_code(cx_order = 'lex'):
+
+def get_bpc_code(cx_order='lex'):
     lift_size, factor = 15, 3
     p1 = [0, 1, 5]  # e + x + x^5
     p2 = [0, 2, 7]  # e + x^2 + x^7
@@ -13,7 +14,8 @@ def get_bpc_code(cx_order = 'lex'):
     code = BpcCode(p1, p2, lift_size, factor)  # Define the BpcCode object
     return init_code(code, cx_order=cx_order)
 
-def get_lpc_code(cx_order = 'lex'):
+
+def get_lpc_code(cx_order='lex'):
     lift_size = 16
     b = np.array([
         [0, 0, 0, 0, 0],  # [e, e, e, e, e]
@@ -23,8 +25,10 @@ def get_lpc_code(cx_order = 'lex'):
     code = QlpCode(b, b, lift_size)  # Define the QlpCode object
     return init_code(code, cx_order=cx_order)
 
+
 def init_code(code, cx_order='lex'):
-    code.build_graph(seed=1)  # Build the Tanner graph and assign directions to its edges.
+    # Build the Tanner graph and assign directions to its edges.
+    code.build_graph(seed=1)
 
     if cx_order == 'lex':
         stabilizer_x = []
@@ -48,10 +52,14 @@ def init_code(code, cx_order='lex'):
         ancilla_type, data_mapping, ancilla_mapping = bpc_code.build_mappings()
 
     elif cx_order == 'theirs':
-        ancilla_mapping = {'Z' + str(i): q for i, q in enumerate(code.zcheck_qubits)}
-        ancilla_mapping.update({'X' + str(i): q for i, q in enumerate(code.xcheck_qubits)})
-        ancilla_type = {name: 'X' if name[0] == 'X' else 'Z' for name in ancilla_mapping.keys()}
-        data_mapping = {q: q for q in set(code.all_qubits) - set(ancilla_mapping.values())}
+        ancilla_mapping = {
+            'Z' + str(i): q for i, q in enumerate(code.zcheck_qubits)}
+        ancilla_mapping.update(
+            {'X' + str(i): q for i, q in enumerate(code.xcheck_qubits)})
+        ancilla_type = {
+            name: 'X' if name[0] == 'X' else 'Z' for name in ancilla_mapping.keys()}
+        data_mapping = {q: q for q in set(
+            code.all_qubits) - set(ancilla_mapping.values())}
 
         cx_list = []
         directions = list(code.direction_inds.keys())
@@ -70,7 +78,8 @@ def init_code(code, cx_order='lex'):
         # map qubit indices to ancilla names or data names in the cx_list
         for i in range(len(cx_list)):
             q1, q2 = cx_list[i]
-            cx_list[i] = (all_qubits_ids_to_names[q1], all_qubits_ids_to_names[q2])
+            cx_list[i] = (all_qubits_ids_to_names[q1],
+                          all_qubits_ids_to_names[q2])
             # change the order so that the data qubit is always the first one
             if cx_list[i][0] in ancilla_mapping:
                 assert cx_list[i][1] in data_mapping
